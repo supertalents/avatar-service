fd935b9ccfee6a15468b47794aca276efa49536e
Ahsan Shaikh <91524954+pyDashNinja@users.noreply.github.com>
2023-03-22 11:54:12 +0500

    Update and rename app.py to controlnet.py

---
+++ controlnet.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+import torch
+from PIL import Image
+
+
+def get_canny_filter(image, low_threshold, high_threshold):
+    
+    if not isinstance(image, np.ndarray):
+        image = np.array(image) 
+        
+    image = cv2.Canny(image, low_threshold, high_threshold)
+    image = image[:, :, None]
+    image = np.concatenate([image, image, image], axis=2)
+    canny_image = Image.fromarray(image)
+    return canny_image
+
+
+def cn(model,prompt, image, height, width , num_inference_steps, scale, seed, low_threshold, high_threshold,  nprompt):
+    generator = torch.manual_seed(seed)
+
+
+    # prompt = "photo of a person holding a cardboard in the hand, ((placeholder)) , ((cardboard in holding in hand))"
+    canny_image = get_canny_filter(image, low_threshold, high_threshold)
+    output = model(
+        prompt=prompt,
+        image=canny_image,
+        negative_prompt=nprompt,
+        height=height,
+        width=width,
+        guidance_scale=scale,
+        generator=generator,
+        num_images_per_prompt=1,
+        num_inference_steps=num_inference_steps,
+        # mask_image=mask
+    )
+    # return [canny_image,output.images[0]]
+    return output.images[0]
