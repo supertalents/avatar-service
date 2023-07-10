5051886bf8a8d05546aee809cab5edf69f2d8be2
Ahsan Shaikh <91524954+pyDashNinja@users.noreply.github.com>
2023-07-10 16:00:05 +0500

    Update env.py

--- env.py
+++ env.py
@@ -1,4 +1,8 @@
 import os
 
+# check if ./cache exists if not create
+if not os.path.exists(os.path.join(os.getcwd(), "cache")):
+    os.makedirs(os.path.join(os.getcwd(), "cache"))
+
 os.environ["XDG_CACHE_HOME"] = os.path.join(os.getcwd(), "cache")
 print("successfully set")
